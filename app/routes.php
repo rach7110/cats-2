a43786423f9fa9178e1c39b5bd41f45ee5f55849
Rachel Loziuk <rachel.loziuk@gmail.com>
2014-04-29 17:09:15 -0500

    added CRUD routing; moved single cat route to below cat/create route so there's no interference; added an edit view

--- app/routes.php
+++ app/routes.php
@@ -12,24 +12,70 @@
 */
 
 Route::get('/', function(){
-	return Redirect::to('cats');
+  return Redirect::to('cats');
 });
 
 Route::get('cats', function(){
-	$cats = Cat::all();
-	return View::make('cats.index')
-		->with('cats', $cats);
+  $cats = Cat::all();
+  return View::make('cats.index')
+    ->with('cats', $cats);
 });
 
 Route::get('cats/breed/{name}', function($name) {
 	$breed = Breed::whereName($name)->with('cats')->first();
 	return View::make('cats.index')
-		->with('breed', $breed)
-		->with('cats', $breed->cats);
+    ->with('breed', $breed)
+    ->with('cats', $breed->cats);
 });
 
-Route::get('cats/{id}', function($id){
-	$cat = Cat::find($id);
-	return View::make('cats.single')
-		->with('cat', $cat);
+// CREATE:
+Route::get('cats/create', function() {
+	$cat = new Cat;
+	return View::make('cats.edit')
+    ->with('cat', $cat)
+    ->with('method', 'post');
+});
+
+// CREATE HANDLER: POST
+Route::post('cats', function(){
+  $cat=Cat::create(Input::all());
+  return Redirect::to('cats/'.$cat->id)
+    ->with('message', 'Succesfully created new cat!');
+});
+
+// SINGLE CAT:
+
+Route::model('cat', 'Cat'); // <- bound model to route.
+// Allows you to shorten the route and pass a 'Cat' object to it instead. Looks like this:
+Route::get('cats/{cat}', function(Cat $cat) {
+  return View::make('cats.single')
+    ->with('cat', $cat);
+});
+
+// EDIT:
+Route::get('cats/{cat}/edit', function(Cat $cat) {
+  return View::make('cats.edit')
+    ->with('cat', $cat) 
+    ->with('method', 'put');
+});
+
+// EDIT HANDLER: 
+Route::put('cats/{cat}', function(Cat $cat){
+  $cat->update(Input::all());
+  return Redirect::to('cats/'.$cat->id)
+    ->with('message', 'Successfully updated $cat->id cat!');
+});
+
+// DELETE:
+Route::get('cats/{cat}/delete', function(Cat $cat) {
+  return View::make('cats.edit')
+    ->with('cat', $cat)
+    ->with('method', 'delete');
+});
+
+// DELETE HANDLER: 
+Route::put('cats/{cat}', function(Cat $cat){
+  $cat->delete();
+  return Redirect::to('cats')
+      ->with('message', 'Successfully deleted page!');
 });
