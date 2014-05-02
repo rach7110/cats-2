afdb0f37d87a6024712ba79768a7203dd036f665
Rachel Loziuk <rachel.loziuk@gmail.com>
2014-05-02 16:10:25 -0500

    added an Auth check and error check to Master view

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -8,13 +8,30 @@
 	<body>
 		<div class="container">
 			<div class="page-header">
+<!-- Display Login/Logout link -->
+				<div class="text-right">
+					@if(Auth::check())
+						Logged in as
+						<strong>{{{auth::user()->username}}}</strong>
+						{{link_to('logout', 'Log Out')}}
+					@else
+						{{link_to('login', 'Log In')}}
+				</div>
 				@yield('header')
 			</div>
+<!-- Display success messages -->
 			@if(Session::has('message'))
 				<div class="alert alert-success">
 					{{Session::get('message')}}
 				</div>
 				@endif
+<!--Display error messages-->
+			@if(Session::has('error'))
+				<div class="alert alert-warning">
+					{{Session::get('error')}}
+				</div>
+				@endif
+<!-- Display Main Content -->
 			@yield('content')
 		</div>
 	</body>
